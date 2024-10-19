c423805bd28f0d8a10bd37e372789c97869caf5c
piyanshugehani <piyanshugehani@gmail.com>
2024-10-20 00:31:55 +0530

    shadcn

--- src/components/ThreeDCardDemo.jsx
+++ src/components/ThreeDCardDemo.jsx
@@ -1,14 +1,10 @@
-import { useRef } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import User from './components/User';
-import ThemeToggle from './components/ThemeToggle';
-import { Button } from "@/components/ui/button";
-import { AnimatedBeam } from './../components/ui/animated-beam';
-import { CardBody, CardContainer, CardItem } from "./../components/ui/3d-card";
+"use client";
 
-function ThreeDCardDemo() {
+import Image from "next/image";
+import React from "react";
+import { CardBody, CardContainer, CardItem } from "../ui/3d-card";
+
+export function ThreeDCardDemo() {
   return (
     <CardContainer className="inter-var">
       <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
@@ -31,10 +27,10 @@ function ThreeDCardDemo() {
           rotateZ={-10}
           className="w-full mt-4"
         >
-          <img
+          <Image
             src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            height="240"
-            width="400"
+            height={1000}
+            width={1000}
             className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
             alt="thumbnail"
           />
@@ -61,39 +57,3 @@ function ThreeDCardDemo() {
     </CardContainer>
   );
 }
-
-
-function App() {
-  // Create refs for the container and two points for the animated beam
-  const containerRef = useRef(null);
-  const div1Ref = useRef(null);
-  const div2Ref = useRef(null);
-
-  return (
-    <>
-      <div className="min-h-screen bg-white dark:bg-black text-gray-800 dark:text-gray-200">
-        <header className="p-4 flex justify-between items-center">
-          <h1 className="text-2xl">Minithon</h1>
-          <div className='flex justify-around'>
-            <ThemeToggle />
-            <User />
-          </div>
-        </header>
-        <main className="p-4" ref={containerRef}>
-          <Button variant="outline">Button</Button>
-          <div ref={div1Ref} className="absolute left-0">{"<"}</div>
-          <div ref={div2Ref} className="absolute right-0">{">"}</div>
-          <AnimatedBeam
-            duration={3}
-            containerRef={containerRef}
-            fromRef={div1Ref}
-            toRef={div2Ref}
-          />
-          <ThreeDCardDemo/>
-        </main>
-      </div>
-    </>
-  );
-}
-
-export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,37 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: ["class"],
-    content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Ensure this path covers all your component files
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
   ],
+  prefix: "",
   theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 }
